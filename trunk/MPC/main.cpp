252ee4b36dfd03db257946f182447a36b9614c37
staessenssibrand@hotmail.com <staessenssibrand@hotmail.com@94d36c20-5ba7-c05f-6ecd-800045c3f762>
2010-05-23 19:11:10 +0000

    no caching of usb data when calibrating

--- trunk/MPC/main.cpp
+++ trunk/MPC/main.cpp
@@ -22,31 +22,14 @@ using namespace video;
 using namespace std;
 
 #define FILENAME "out.csv"
+#define TIMEOUT_FOR_CALIBRATION 8000
+typedef enum GAME_STAGE {
+	CALIBRATE_BACK = 0, CALIBRATE_FRONT, IN_GAME
+} GAME_STAGE;
 
 #ifdef _MSC_VER
 #pragma comment(lib, "Irrlicht.lib")
 #endif
-
-void calibrate(USB_Controller* usbController, LocationTracker* locationTracker,
-		GUIManager* guiMan) {
-	int values[3];
-
-	// back
-	guiMan->setText(GUI_ID_GAME_MESSAGE,
-			"Point the top of the rod to the ground behind you.\n");
-	sleep(8);
-	usbController->update();
-	locationTracker->calibrateBack(usbController->get_accelY());
-
-	// front
-	guiMan->setText(GUI_ID_GAME_MESSAGE,
-			"Point the top of the rod to the ground in front of you.\n");
-	sleep(8);
-	usbController->update();
-	locationTracker->calibrateFront(usbController->get_accelY());
-
-	locationTracker->reset();
-}
 
 std::string intToString(int val) {
 	std::ostringstream stm;
@@ -97,25 +80,25 @@ void addUSBControllerValuesToFile(USB_Controller* usbController,
 int main() {
 	srand(time(NULL));
 
-	IrrlichtDevice *device = createDevice(EDT_OPENGL, core::dimension2d<u32>(
-			SCREEN_WIDTH, SCREEN_HEIGTH), 16, false, true, false);
-	video::IVideoDriver* driver = device->getVideoDriver();
-	scene::ISceneManager* smgr = device->getSceneManager();
-	gui::IGUIEnvironment* guienv = device->getGUIEnvironment();
+	IrrlichtDevice *pdevice = createDevice(EDT_OPENGL, core::dimension2d<u32>(
+			SCREEN_WIDTH, SCREEN_HEIGTH), 16, true, true, false);
+	video::IVideoDriver* pdriver = pdevice->getVideoDriver();
+	scene::ISceneManager* psmgr = pdevice->getSceneManager();
+	gui::IGUIEnvironment* pguienv = pdevice->getGUIEnvironment();
 
 	KeyEventReceiver receiver;
-	device->setEventReceiver(&receiver);
-	SceneManager sceneMan(device, driver, smgr);
-	ICameraSceneNode * camera = sceneMan.init();
-	Cross cross(driver, smgr, sceneMan.getTerrainSelector());
-	FishManager fishMan(device, driver, smgr);
+	pdevice->setEventReceiver(&receiver);
+	SceneManager sceneMan(pdevice, pdriver, psmgr);
+	ICameraSceneNode * pcamera = sceneMan.init();
+	Cross cross(pdriver, psmgr, sceneMan.getTerrainSelector());
+	FishManager fishMan(pdevice, pdriver, psmgr);
 	fishMan.addFish(SHARK, vector3df(5725.f, DEFAULT_DEPTH_FISH, 5655.f),
 			vector3df(3635.f, DEFAULT_DEPTH_FISH, 6235.f), 15000);
 	fishMan.addFish(CLOWN, vector3df(3387.f, DEFAULT_DEPTH_FISH, 6024.f),
 			vector3df(4350.f, DEFAULT_DEPTH_FISH, 3750.f), 35000);
-	GUIManager guiMan(device);
+	GUIManager guiMan(pdevice);
 	LocationTracker locationTracker;
-	ActionManager actionMan(camera, &locationTracker, &cross, &fishMan);
+	ActionManager actionMan(pcamera, &locationTracker, &cross, &fishMan);
 	receiver.setActionManager(&actionMan);
 
 	USB_Controller usbController(&guiMan);
@@ -131,78 +114,122 @@ int main() {
 	//                             Main loop
 	/////////////////////////////////////////////////////////////////////////
 	int lastFPS = -1;
-	u32 then = device->getTimer()->getTime();
-	u32 now = device->getTimer()->getTime();
+	u32 then = pdevice->getTimer()->getTime();
+	u32 now = pdevice->getTimer()->getTime();
+	u32 timeout = now + TIMEOUT_FOR_CALIBRATION;
 
+	int accelY;
+	bool switchOn;
+	int encoderValue;
+	int potentioValue;
+	bool pushButtonOn;
+
+	GAME_STAGE stage = CALIBRATE_BACK;
 	printUSBControllerValues(&usbController, &guiMan);
 	resetFile();
-	while (device->run() && !receiver.getStopApp())
-		if (device->isWindowActive()) {
-			now = device->getTimer()->getTime();
+	actionMan.reset();
+
+	while (pdevice->run() && !receiver.getStopApp())
+		if (pdevice->isWindowActive()) {
+			now = pdevice->getTimer()->getTime();
 			unsigned int lastFrameDurationMilliSeconds = now - then;
 			then = now;
 
 			// retrieve serial values
 			if (arduinoInputAvailable && usbController.update() > 8) {
-				int accelY = usbController.get_accelY();
-				bool switchOn = usbController.switch_on();
-				int encoderValue = usbController.get_rotation_value();
-				int potentioValue = usbController.get_potentiometer_value();
-				bool pushButtonOn = usbController.push_on();
-
 				printUSBControllerValues(&usbController, &guiMan);
 				addUSBControllerValuesToFile(&usbController, now);
-				actionMan.insertArduinoValues(accelY, switchOn, encoderValue,
-						potentioValue, pushButtonOn);
 			}
 
-			addUSBControllerValuesToFile(&usbController, now);
-			actionMan.update(lastFrameDurationMilliSeconds, now);
+			switch (stage) {
+			case CALIBRATE_BACK:
+				guiMan.setText(GUI_ID_GAME_MESSAGE,
+						"Point the top of the rod to the ground behind you.\n");
 
-			if (actionMan.isHooked()) {
+				if (now > timeout) {
+					timeout = now + TIMEOUT_FOR_CALIBRATION;
+					stage = CALIBRATE_FRONT;
+					if (arduinoInputAvailable)
+						locationTracker.calibrateBack(
+								usbController.get_accelY());
+				}
+				break;
+			case CALIBRATE_FRONT:
 				guiMan.setText(GUI_ID_GAME_MESSAGE,
-						"Gotcha !\nA fish has been hooked, bring it in!");
-			} else if (actionMan.isBroken()) {
-				guiMan.setText(GUI_ID_GAME_MESSAGE,
-						"Oh no, your line has broken, don't be so greedy!\nRebegin/reset... .\n");
-			} else if (actionMan.isLandedWithFish()) {
-				guiMan.setText(GUI_ID_GAME_MESSAGE,
-						"Yay, the fish has been landed!\nRebegin/reset... .\n");
-			} else if (actionMan.isLandedWithNoFish()) {
-				guiMan.setText(GUI_ID_GAME_MESSAGE,
-						"No more line to bring in.\nRebegin/reset... .\n");
-			} else if (actionMan.throwBlocked()) {
-				guiMan.setText(
-						GUI_ID_GAME_MESSAGE,
-						"Oh no, your line was blocked while throwing!\nRelease the line before throwing! Rebegin/reset... .\n");
-			} else
-				guiMan.setText(GUI_ID_GAME_MESSAGE, "Happy fishing! :)\n");
+						"Point the top of the rod to the ground in front of you.\n");
 
-			if (actionMan.checkBuzz(now) && arduinoInputAvailable)
-				usbController.buzz(100);
+				if (now > timeout) {
+					timeout = now + TIMEOUT_FOR_CALIBRATION;
+					stage = IN_GAME;
+					if (arduinoInputAvailable)
+						locationTracker.calibrateFront(
+								usbController.get_accelY());
+					locationTracker.reset();
+					actionMan.reset();
+				}
+				break;
+			case IN_GAME:
+				if (arduinoInputAvailable) {
+					accelY = usbController.get_accelY();
+					switchOn = usbController.switch_on();
+					encoderValue = usbController.get_rotation_value();
+					potentioValue = usbController.get_potentiometer_value();
+					pushButtonOn = usbController.push_on();
+
+					actionMan.insertArduinoValues(accelY, switchOn,
+							encoderValue, potentioValue, pushButtonOn);
+				}
+				actionMan.update(lastFrameDurationMilliSeconds, now);
+
+				if (actionMan.isHooked()) {
+					guiMan.setText(GUI_ID_GAME_MESSAGE,
+							"Gotcha !\nA fish has been hooked, bring it in!");
+				} else if (actionMan.isBroken()) {
+					guiMan.setText(GUI_ID_GAME_MESSAGE,
+							"Oh no, your line has broken, don't be so greedy!\nRebegin/reset... .\n");
+				} else if (actionMan.isLandedWithFish()) {
+					guiMan.setText(GUI_ID_GAME_MESSAGE,
+							"Yay, the fish has been landed!\nRebegin/reset... .\n");
+				} else if (actionMan.isLandedWithNoFish()) {
+					guiMan.setText(GUI_ID_GAME_MESSAGE,
+							"No more line to bring in.\nRebegin/reset... .\n");
+				} else if (actionMan.throwBlocked()) {
+					guiMan.setText(
+							GUI_ID_GAME_MESSAGE,
+							"Oh no, your line was blocked while throwing!\nRelease the line before throwing! Rebegin/reset... .\n");
+				} else
+					guiMan.setText(GUI_ID_GAME_MESSAGE, "Happy fishing! :)\n");
+
+				if (actionMan.checkBuzz(now) && arduinoInputAvailable)
+					usbController.buzz(100);
+				break;
+			default:
+				cerr << "Unknown game stage.\n";
+				break;
+			}
 
 			/*cout << "CAM: " << camera->getAbsolutePosition().X << " : "
 			 << camera->getAbsolutePosition().Y << " : "
 			 << camera->getAbsolutePosition().Z << endl;*/
 
-			driver->beginScene(true, true, video::SColor(0, 200, 200, 200));
-			smgr->drawAll();
-			guienv->drawAll();
-			driver->endScene();
+			pdriver->beginScene(true, true, video::SColor(0, 200, 200, 200));
+			psmgr->drawAll();
+			pguienv->drawAll();
+			pdriver->endScene();
 
 			//Shows FPS in the title screen
-			int fps = driver->getFPS();
+			int fps = pdriver->getFPS();
 			if (lastFPS != fps) {
 				core::stringw str = L"Fisher's Price [";
-				str += driver->getName();
+				str += pdriver->getName();
 				str += "] FPS:";
 				str += fps;
-				device->setWindowCaption(str.c_str());
+				pdevice->setWindowCaption(str.c_str());
 				lastFPS = fps;
 			}
 		} else {
-			device->yield();
+			pdevice->yield();
 		}
-		device->drop();
+		pdevice->drop();
 		return 0;
 	}
